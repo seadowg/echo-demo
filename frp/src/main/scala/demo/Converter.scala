5f49393e8df2a2390100676c5bdbec68b0c256d1
Callum Stott <oetzi101@gmail.com>
2012-04-28 16:45:48 +0100

    Add Slider to FRP demo
    The FRP demo now has a Slider to change the conversion rate.

--- frp/src/main/scala/demo/Converter.scala
+++ frp/src/main/scala/demo/Converter.scala
@@ -7,17 +7,21 @@ import com.github.oetzi.echo.display._
 object Converter extends EchoApp {
   def setup(args: Array[String]) {
     val field = Field()
-    val result = Text(field.text.map(v => string2Double(v, d => d * 1.6)))
+    val slider = Slider()
+    val result = Text(field.text.map2(slider.value) {
+      (v,s) => string2Double(v, d => d * (1 + (s.toDouble / 100)))
+    })
     
     val frame = Frame(200, 200, List(
       field,
+      slider,
       result
     ))
   }
   
   private def string2Double(string: String, func: Double => Double): String = {
     try {
-      func(string.toDouble).toString
+      "%.2f".format(func(string.toDouble)).toString
     }
     
     catch {
